e670406f6581573356d7c0bcb5dabf5872fbbca0
prettyfoam <1334875424@QQ.com>
2018-11-03 23:58:15 +0800

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -83,8 +83,8 @@ async function start(num) {
     let page = 1 //当前所在页面
     let data = [] //获取链接总数
     while (storage.length < num) {
-        //控制并发，高于1000易封IP
-        while (data.length < 700) {
+        //控制并发，高于500易封IP
+        while (data.length < 300) {
             let currentData = await get(page++) //当前链接
             data = data.concat(currentData)
         }
